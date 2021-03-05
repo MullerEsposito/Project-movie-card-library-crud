9c569e825ea71e83267bb3b9c892acbcbba4ccac
Muller Esposito <mulleresposito@hotmail.com>
2021-03-05 04:32:39 -0300

    refactor(MovieList);

---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
+
+import * as movieAPI from '../services/movieAPI';
+import './MovieList.css';
+
+class MovieList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+      isLoading: true,
+    };
+  }
+
+  async componentDidMount() {
+    await this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    this.setState({ isLoading: true }, async () => {
+      const movies = await movieAPI.getMovies();
+      this.setState({ movies, isLoading: false });
+    });
+  }
+
+  render() {
+    const { movies, isLoading } = this.state;
+
+    if (isLoading) return <Loading />;
+
+    return (
+      <div className="container-movie-list" data-testid="movie-list">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
